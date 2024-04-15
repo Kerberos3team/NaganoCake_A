cc062af0407474f41a33e45a9e7502a2ff5b2320
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-04-15 10:33:26 +0000

    [add]genre create,edit,update

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,23 +3,27 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.new
     @genres = Genre.all
   end
-  
+
   def create
     genre = Genre.new(genre_params)
     genre.save
-    redirect_to :index
+    redirect_to admin_genres_path
   end
-  
+
   def edit
+    @genre = Genre.find(params[:id])
   end
-  
+
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path, notice: "変更を保存しました。"
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
-  
+
 end
