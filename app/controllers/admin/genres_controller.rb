d25983848d3ba3df88271826ec5a1753e02c7225
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-04-15 09:08:43 +0000

    [add]genre controller view

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,25 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
-
+  
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to :index
+  end
+  
   def edit
   end
+  
+  def update
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
