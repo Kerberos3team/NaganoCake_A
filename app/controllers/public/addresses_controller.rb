76de2fae3ee33021b1d7b35d2fe40642993d805a
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-16 12:31:53 +0000

    routing error

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -38,7 +38,7 @@ class Public::AddressesController < ApplicationController
   private
   
   def address_params
-    params.require(:address).permit(:name, :postal_code, :address)
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
     
   end
 end
