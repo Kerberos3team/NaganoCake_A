4c163c63f93d4d0964a1f1cfe1f96f917e8adbb9
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-04-16 05:46:48 +0000

    [add]index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,7 +7,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to item_path(@item), notice: "商品を登録しました。"
+      redirect_to admin_item_path(@item), notice: "商品を登録しました。"
     else
       @item = Item.new
       render "new"
@@ -19,15 +19,26 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update
+      redirect_to admin_item_path(@item), notice: "商品情報を更新しました。"
+    else
+      render "edit"
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :status)
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
   has_many :cart_items
 
   has_one_attached :image
-  
+
   validates :image, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
   validates :genre_id, presence: true
-  validates :status, presence: true
-  
+  validates :is_active, presence: true
+
 end
