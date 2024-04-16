66fdeb791b59c4862c761dfe6ef70d04305cf203
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-16 12:04:37 +0000

    routing error

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,44 @@
 class Public::AddressesController < ApplicationController
+  #before_action :authenticate_customer!
+  
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path 
+      flash[:success] = '登録しました。'
+    else
+      @address = Address.new
+      @addresses = current_customer.addresses
+      render 'index'
+    end  
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+  
+  def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
+  end
+  
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+    
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ApplicationRecord
   
   belongs_to :customer
   
+  validates :postal_code, length: {is: 7}, numericality: { only_integer: true }, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+
 end
