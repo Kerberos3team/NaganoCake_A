a94d19cb7a87df3d37a22e2bcf7bbf55471980fc
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-16 12:43:55 +0000

    errorすり合わせ前

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,44 +1,44 @@
 class Public::AddressesController < ApplicationController
   #before_action :authenticate_customer!
-  
+
   def index
     @address = Address.new
     @addresses = current_customer.addresses
   end
-  
+
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     if @address.save
-      redirect_to addresses_path 
+      redirect_to addresses_path
       flash[:success] = '登録しました。'
     else
       @address = Address.new
       @addresses = current_customer.addresses
       render 'index'
-    end  
+    end
   end
 
   def edit
     @address = Address.find(params[:id])
   end
-  
+
   def update
     address = Address.find(params[:id])
     address.update(address_params)
     redirect_to addresses_path
   end
-  
+
   def destroy
     address = Address.find(params[:id])
     address.destroy
     redirect_to addresses_path
   end
-  
+
   private
-  
+
   def address_params
     params.require(:address).permit(:customer_id, :name, :postal_code, :address)
-    
+
   end
 end
