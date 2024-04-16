75725f0a268661543b7fb5a1ed5b435270f2e9e0
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-04-16 03:55:31 +0000

    [fix]item new layout

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,14 +4,30 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item), notice: "商品を登録しました。"
+    else
+      @item = Item.new
+      render "new"
+    end
+  end
+
   def index
     @items = Item.all
   end
-  
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :status)
   end
 
 
