3e5670dbe644b0bee734e6260e5e916d186fd807
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-04-16 02:39:42 +0000

    [add]item new view

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,11 @@ class Item < ApplicationRecord
   has_many :cart_items
 
   has_one_attached :image
-
+  
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :genre_id, presence: true
+  validates :status, presence: true
+  
 end
