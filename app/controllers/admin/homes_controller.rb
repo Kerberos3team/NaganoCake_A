798800d405becbe4c5c9a98ad7383ac5d4d73d78
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-04-16 07:14:49 +0000

    [Fix]商品一覧の表示

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
+    @items = Item.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  5.times do |n|
+    Item.create!(
+      genre_id: 1,
+      name: "美味しいケーキ#{n + 1}",
+      introduction: "サンプルテキストさんぷるてきすとサンプルテキストさんぷるてきすと",
+      price: 3000,
+      is_active: true
+    )
+  end
