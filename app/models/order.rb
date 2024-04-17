cb9f00e46e8b2d73672c6dc34fe6c0a7e5374609
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-17 05:56:18 +0000

    enum columnadd

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ApplicationRecord
   has_many :order_details
   
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting: 0, paid:1, producting:2, preparing:3, sented:4 }
+  
 end
