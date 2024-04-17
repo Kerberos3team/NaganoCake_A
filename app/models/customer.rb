0bdbd81a98c58012ddd9c9f3c80f11307457f51d
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-17 05:01:54 +0000

    customer validate

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,5 +7,14 @@ class Customer < ApplicationRecord
  has_many :cart_items
  has_many :orders
  has_many :addresses
+ 
+ validates :last_name, presence: true
+ validates :first_name, presence: true
+ validates :last_name_kana, presence: true
+ validates :first_name_kana, presence: true
+ validates :postal_code, presence: true
+ validates :address, presence: true
+ validates :telephone_number, presence: true
+ validates :is_active, inclusion: {in: [true, false]}
 
 end
