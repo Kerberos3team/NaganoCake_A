4dce5fea5910fb2d316ebd399b891b42f3817b93
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-18 01:21:32 +0000

    flash add

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,12 +17,6 @@ class Customer < ApplicationRecord
  validates :telephone_number, presence: true
  validates :is_active, inclusion: {in: [true, false]}
  
-  #def full_name
-    #"#{last_name} #{first_name}"
-  #end
-  
-  #def full_name_kana
-    #"#{last_name_kana} #{first_name_kana}"
-  #end
+
 
 end
