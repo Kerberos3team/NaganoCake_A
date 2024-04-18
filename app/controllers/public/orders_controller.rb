11febf54efb4f728ade23387613e545f0eafb821
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-04-18 03:34:00 +0000

    [Update]注文機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,14 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+  @order = Order.new(order_params)
+  @order.postal_code = current_customer.postal_code
+  @order.address = current_customer.address
+  @order.name = current_customer.first_name + current_customer.last_name
+  end
+
+  def create
+    @order = Order.new(order_params)
   end
 
   def thanks
@@ -14,5 +22,9 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
