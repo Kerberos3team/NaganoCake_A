cc65aead2ca7a5cf54788a37d42483c2d938e111
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-18 01:47:58 +0000

    flash update

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,33 +1,32 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def edit
     @customer = Customer.find(current_customer.id)
   end
-  
+
   def update
     @customer = Customer.find(current_customer.id)
     if @customer.update(customer_params)
       flash[:notice] = "会員情報更新に成功しました。"
       redirect_to customers_my_page_path
     else
-      flash.now[:alert] = "会員情報更新に失敗しました。"
       render 'edit'
-    end  
-  end  
+    end
+  end
 
   def unsubscribe
   end
 
   def withdraw
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
   end
