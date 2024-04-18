1a92c09235687000696dc37483eb82458a63ddfa
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-04-18 02:16:34 +0000

    [Update]国際化APIi18nの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ApplicationRecord
-  
+
   belongs_to :customer
-  
+
+  def address_display
+    '〒' + postal_code + ' ' + address + '　' + name
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
-  
+
   belongs_to :customer
   has_many :order_details
-  
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+
 end
