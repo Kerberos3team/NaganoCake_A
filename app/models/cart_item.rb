8ff465c3883e56ba29709101f3f8912234639d0a
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-18 05:58:46 +0000

    comp

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,13 +1,12 @@
 class CartItem < ApplicationRecord
-  before_action :authenticate_customer!
-  
+
   belongs_to :customer
   belongs_to :item
 
   validates :amount, presence: true
-
+  
   def subtotal
     item.with_tax_price * amount
   end
-
+  
 end
