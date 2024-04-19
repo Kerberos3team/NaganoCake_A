0d7a367e1999298c2fe7b27464f2d45e1bdcb9f5
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-19 06:19:07 +0000

    レイアウト調整

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,11 +3,13 @@ class Public::ItemsController < ApplicationController
   def index
     @items_all_counter = Item.all
     @items = Item.all.order(created_at: :desc).page(params[:page])
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 
 end
