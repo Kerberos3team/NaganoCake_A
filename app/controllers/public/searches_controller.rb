8876263a0e29d67848bebcc3d86c979d5ff4c662
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-04-20 10:05:58 +0000

    [modify]search form layout

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,13 +4,15 @@ class Public::SearchesController < ApplicationController
   def genre_search
     @genre = Genre.find(params[:genre_id])
     @genre_id = params[:genre_id]
-    @items = Item.where(genre_id: @genre_id)
+    @items = Item.where(genre_id: @genre_id).page(params[:page])
     @genres = Genre.all
-  end  
+  end
 
   def search
+    @genres = Genre.all
     @keyword = params[:keyword]
-    @results = Item.search_for(@keyword)
+    @results_all = Item.search_for(@keyword)
+    @results = @results_all.page(params[:page])
   end
 
 end
