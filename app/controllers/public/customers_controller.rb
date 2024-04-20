fd79f837140054743cee793d0950235ff1ba4679
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-20 07:14:13 +0000

    admin/customer fix

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -33,6 +33,6 @@ class Public::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
