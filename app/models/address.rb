ac71cfc95ce078f4e4a41d725e79cd21ff03f739
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-21 07:43:18 +0000

    order/address comp

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,8 +8,13 @@ class Address < ApplicationRecord
   end
 
   validates :postal_code, length: {is: 7 }, numericality: { only_integer: true }, presence: true
-  validates :address, presence: true
   validates :name, presence: true
+  validates :address, presence: true
+  validate :address_must_contain_letters, if: -> { address.present? }
 
-
+  def address_must_contain_letters
+    if address.match?(/\A[0-9]+\z/)
+      errors.add(:address, "は数字のみではなく文字を含めてください")
+    end
+  end
 end
