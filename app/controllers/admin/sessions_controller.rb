d16033c63a2db5dcae7353b345d5512c84c939b4
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-04-24 11:32:26 +0000

    admin/login

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -28,8 +28,7 @@ class Admin::SessionsController < Devise::SessionsController
   protected
 
   def after_sign_in_path_for(resource)
-    #注文履歴一覧？とりあえず、、、
-    admin_items_path
+    admin_path
   end
 
   def after_sign_out_path_for(resource)
